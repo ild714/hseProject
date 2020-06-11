d96abbe27cc3d39a05b1d3a2d071015db6b28a11
ildar714 <ildarnigmetzjanov@gmail.com>
2020-06-12 00:23:43 +0300

    Create the top view for first controller

---
+++ hseProject/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hseProject
+//
+//  Created by Ильдар Нигметзянов on 11.06.2020.
+//  Copyright © 2020 Ildar Nigmetzyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
