13f30de5bef773249b5fbdae10ff2dbd23279db2
ildar714 <ildarnigmetzjanov@gmail.com>
2020-07-28 18:59:17 +0300

    Change design

---
+++ hseProject_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hseProject_1
+//
+//  Created by Ильдар Нигметзянов on 28.07.2020.
+//  Copyright © 2020 Ildar Nigmetzyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
