cf72931fad0160a68ea6b07fd716b36a4a59894d
ildar714 <ildarnigmetzjanov@gmail.com>
2020-08-11 22:06:30 +0300

    Delete first project

---
+++ Podfile
@@ -0,0 +1,7 @@
+
+platform :ios, '9.0'
+use_frameworks!
+
+target 'hseProject_1' do
+    pod 'SwiftyJSON', '~> 4.0'
+end
---
+++ Pods/Target Support Files/Pods-hseProject_1/Pods-hseProject_1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hseProject_1 : NSObject
+@end
+@implementation PodsDummy_Pods_hseProject_1
+@end
