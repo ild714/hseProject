0c5c9b317c04f3f29fe97fbd6561e3bd75850e79
ildar714 <ildarnigmetzjanov@gmail.com>
2020-12-05 14:38:18 +0300

    Added functionality for ScriptServiceView 70%

--- hseProject_1/ViewController/ScriptServiceViewController/ScriptServiceTableViewCell.swift
+++ hseProject_1/ViewController/ScriptServiceViewController/ScriptServiceTableViewCell.swift
@@ -10,8 +10,64 @@ import UIKit
 
 class ScriptServiceTableViewCell: UITableViewCell {
 
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var co2Label: UILabel!
+    @IBOutlet weak var stack: UIStackView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
-    }    
+//        stack.alignment = UIStackView.Alignment.center
+//        stack.spacing = 3
+//        stack = nil
+//        stack.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func congigure(serviveScript: ServiceScript, number: Int){
+        stack.axis = NSLayoutConstraint.Axis.horizontal
+        stack.distribution = UIStackView.Distribution.fillEqually
+        stack.spacing = 3
+        
+        stack.subviews.forEach { (view) in
+            view.removeFromSuperview()
+        }
+        
+        self.numberLabel.text = "\(number)."
+        self.temperatureLabel.text = "\(serviveScript.temperature)"
+        self.humidityLabel.text = "\(serviveScript.humidity)"
+        self.co2Label.text = "--"
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        self.timeLabel.text = ("\(formatter.string(from: serviveScript.time))")
+        
+        if serviveScript.radiatorOn {
+            let imageRadiator = UIImageView(image: UIImage(named: "radiator"))
+            imageRadiator.heightAnchor.constraint(equalToConstant: 25).isActive = true
+            imageRadiator.widthAnchor.constraint(equalToConstant: 25).isActive = true
+            stack.addArrangedSubview(imageRadiator)
+        }
+        if serviveScript.hotFloorOn {
+            let imageHotFloor = UIImageView(image: UIImage(named: "hotfloor"))
+            imageHotFloor.heightAnchor.constraint(equalToConstant: 25).isActive = true
+            imageHotFloor.widthAnchor.constraint(equalToConstant: 25).isActive = true
+            stack.addArrangedSubview(UIImageView(image: UIImage(named: "hotfloor")))
+        }
+        if serviveScript.humidifierOn {
+            let imageHumidifier = UIImageView(image: UIImage(named: "humidifier"))
+            imageHumidifier.heightAnchor.constraint(equalToConstant: 25).isActive = true
+            imageHumidifier.widthAnchor.constraint(equalToConstant: 25).isActive = true
+            stack.addArrangedSubview(imageHumidifier)
+        }
+        if serviveScript.conditionerOn {
+            let imageAirConditioner = UIImageView(image: UIImage(named: "air_conditioner"))
+            imageAirConditioner.heightAnchor.constraint(equalToConstant: 25).isActive = true
+            imageAirConditioner.widthAnchor.constraint(equalToConstant: 25).isActive = true
+            stack.addArrangedSubview(imageAirConditioner)
+        }
+    }
 }
--- hseProject_1/ViewController/ScriptServiceViewController/ScriptServiceViewController.swift
+++ hseProject_1/ViewController/ScriptServiceViewController/ScriptServiceViewController.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
+struct ServiceScript {
+    var time: Date = Date()
+    var temperature: Int = 0
+    var humidity: Int = 0
+    var radiatorOn = true
+    var hotFloorOn = true
+    var humidifierOn = true
+    var conditionerOn = true
+}
+
 class ScriptServiceViewController: UIViewController {
 
     @IBOutlet weak var serviceLabel: UILabel!
     @IBOutlet weak var imageStack: UIStackView!
     @IBOutlet weak var settingCreator: ViewCustomClass!
+    @IBOutlet weak var temperatureLabel: UITextField!
     @IBOutlet weak var timePicker: UIDatePicker!
+    @IBOutlet weak var humidityLabel: UITextField!
     
     private let pickerView: UIPickerView = UIPickerView(frame: CGRect(x: 0.0, y: 300.0, width: 100.0, height: 300.0))
     
@@ -45,6 +57,134 @@ class ScriptServiceViewController: UIViewController {
         tableView.bottomAnchor.constraint(equalTo: settingCreator.topAnchor,constant: -15).isActive = true
     }
     
+    // Sound changer
+    @IBOutlet weak var soundImage: UIButton!
+    var soundTurnOn = true
+    @IBAction func soundOnOff(_ sender: Any) {
+        if soundTurnOn == true {
+            soundImage.setImage(UIImage(named: "mute"), for: .normal)
+            soundTurnOn.toggle()
+        } else {
+            soundImage.setImage(UIImage(named: "volume"), for: .normal)
+            soundTurnOn.toggle()
+        }
+    }
+    
+    @IBOutlet weak var houseImage: UIButton!
+    @IBOutlet weak var temperatureImage: UIImageView!
+    @IBOutlet weak var humidityImage: UIImageView!
+    var houseTurnOn = true
+    @IBAction func houseOnOff(_ sender: Any) {
+        if houseTurnOn == true {
+            houseImage.setImage(UIImage(named: "not_home"), for: .normal)
+            temperatureImage.image = UIImage(named: "темп_min")
+            humidityImage.image = UIImage(named: "влажность_max")
+            self.humidityLabel.text = "40"
+            self.temperatureLabel.text = "24"
+            houseTurnOn.toggle()
+        } else {
+            houseImage.setImage(UIImage(named: "home"), for: .normal)
+            temperatureImage.image = UIImage(named: "темп_б")
+            humidityImage.image = UIImage(named: "влажность_б")
+            houseTurnOn.toggle()
+        }
+    }
+    
+    @IBOutlet weak var radiatorImage: UIButton!
+    var radiatorTurnOn = true
+    @IBAction func radiatorOnOff(_ sender: Any) {
+        if radiatorTurnOn == true {
+            radiatorImage.setImage(UIImage(named: "not_radiator"), for: .normal)
+            radiatorTurnOn.toggle()
+        } else {
+            radiatorImage.setImage(UIImage(named: "radiator"), for: .normal)
+            radiatorTurnOn.toggle()
+        }
+    }
+    
+    @IBOutlet weak var floorImage: UIButton!
+    var floorTurnOn = true
+    @IBAction func floorOnOff(_ sender: Any) {
+        if floorTurnOn == true {
+            floorImage.setImage(UIImage(named: "not_hotfloor"), for: .normal)
+            floorTurnOn.toggle()
+        } else {
+            floorImage.setImage(UIImage(named: "hotfloor"), for: .normal)
+            floorTurnOn.toggle()
+        }
+    }
+    
+    
+    @IBOutlet weak var humidifierImage: UIButton!
+    var humidifierTurnOn = true
+    @IBAction func humidifierOnOff(_ sender: Any) {
+        if humidifierTurnOn == true {
+            humidifierImage.setImage(UIImage(named: "not_humidifier"), for: .normal)
+            humidifierTurnOn.toggle()
+        } else {
+            humidifierImage.setImage(UIImage(named: "humidifier"), for: .normal)
+            humidifierTurnOn.toggle()
+        }
+    }
+    
+    @IBOutlet weak var conditionerImage: UIButton!
+    var conditionerTurnOn = true
+    @IBAction func conditionerOnOff(_ sender: Any) {
+        if conditionerTurnOn == true {
+           conditionerImage.setImage(UIImage(named: "not_air_conditioner"), for: .normal)
+            conditionerTurnOn.toggle()
+        } else {
+           conditionerImage.setImage(UIImage(named: "air_conditioner"), for: .normal)
+            conditionerTurnOn.toggle()
+        }
+    }
+    
+    var serviceScripts: [ServiceScript] = []
+    
+    @IBAction func addScript(_ sender: Any) {
+        var serviceScript = ServiceScript()
+        if let temperatureLabelInt = Int(temperatureLabel.text ?? "error") {
+            if temperatureLabelInt > 50 || temperatureLabelInt < 15 {
+                let alert = UIAlertController(title: "Некорректное значение для температуры", message: "Ввведите снова показание температуры", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true)
+                return
+            } else {
+                serviceScript.temperature = temperatureLabelInt
+            }
+        } else {
+            let alert = UIAlertController(title: "Ошибка ввода температуры", message: "Ввведите снова теипературу", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        if let humidityLabelInt = Int(humidityLabel.text ?? "error") {
+            if humidityLabelInt < 30 || humidityLabelInt > 80 {
+                let alert = UIAlertController(title: "Некорректное значение для влажности", message: "Ввведите снова показание влжаности", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true)
+                return
+            } else {
+                serviceScript.humidity = humidityLabelInt
+            }
+        } else {
+            let alert = UIAlertController(title: "Ошибка ввода влажности", message: "Ввведите снова влажность", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        serviceScript.conditionerOn = conditionerTurnOn
+        serviceScript.hotFloorOn = floorTurnOn
+        serviceScript.humidifierOn = humidifierTurnOn
+        serviceScript.radiatorOn = radiatorTurnOn
+        serviceScript.time = timePicker.date
+        
+        serviceScripts.append(serviceScript)
+        tableView.reloadData()
+    }
+    
     let cellIdentifier = String(describing: ScriptServiceTableViewCell.self)
     
     static func storyboardInstance() -> ScriptServiceViewController? {
@@ -61,13 +201,14 @@ class ScriptServiceViewController: UIViewController {
 extension ScriptServiceViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return serviceScripts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ScriptServiceTableViewCell else {
             return UITableViewCell()
         }
+        cell.congigure(serviveScript: serviceScripts[indexPath.row], number: indexPath.row + 1)
         
         return cell
     }
